adaebd932d047c89f80107210e70a9ad9a217bae
Blake Gilmartin <blakegilmartin@Blakes-MacBook-Pro.local>
2024-09-23 13:09:07 -0700

    feat: added middleware, initial user routes and db

--- index.ts
+++ index.ts
@@ -1,14 +1,39 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
+import userRouter from "./src/routes/user";
+import bodyParser from "body-parser";
+import methodOverride from "method-override";
+
+export const errorHandler = async (
+  err: Error,
+  req: Request,
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: NextFunction
+) => {
+  console.error(err);
+  res
+    .status(500)
+    .send({ errors: [{ message: err.message }] })
+    .end();
+};
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/latest", (req: Request, res: Response) => {
-  res.send("This will return the latest connection game");
-});
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+app.use("/user", userRouter);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
---
+++ src/db/userDb.ts
@@ -0,0 +1,25 @@
+import { PrismaClient, Prisma } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const create = async (userData: Prisma.UserCreateInput) => {
+  const user = await prisma.user.create({ data: userData });
+  return user;
+};
+
+const readById = async (id: number) => {
+  const user = await prisma.user.findFirst({ where: { id } });
+  return user;
+};
+
+const deleteById = async (id: number) => {
+  const deleteRes = await prisma.user.delete({ where: { id } });
+  return deleteRes;
+};
+
+const deleteAll = async () => {
+  const deleteAllRes = await prisma.user.deleteMany({});
+  return deleteAllRes;
+};
+
+export default { create, readById, deleteById, deleteAll };
